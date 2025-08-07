3267f2b96c486d74fa863b67b41650587f76f9ac
Kwabena Nyamekye Mawuli <knmawuli@st.ug.edu.gh>
2025-08-08 00:55:31 +0100

    Abstract classes and Interface implementations

---
+++ abstract_classes_and_methods/abstract_classes_and_methods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ShapeExample
+{
+    // Abstract base class
+    abstract class Shape
+    {
+        // Abstract method - must be implemented by derived classes
+        public abstract double GetArea();
+    }
+
+    // Circle class
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    // Rectangle class
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);        
+            Shape rectangle = new Rectangle(4, 6); 
+
+            Console.WriteLine($"Circle area: {circle.GetArea()}");
+            Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        }
+    }
+}
---
+++ oop_interfaces_application/oop_interfaces_application/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MovableExample 
+{
+
+    interface IMovable
+    {
+        void Move();
+    }
+    //Car implements IMovable 
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+     //Bicycle implements IMovable
+    class Bicycle : IMovable
+            {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+        
+        }
+    
+            
+    class Program {
+        static void Main(string[] args) 
+        {
+            //Create instances 
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+
+            //Call Move method
+            myCar.Move();
+            myBicycle.Move();
+        }
+    }
+
+}
