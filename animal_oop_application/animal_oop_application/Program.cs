012ba3d4583fbec5256ed9dd2954b303a4062e05
Kwabena Nyamekye Mawuli <knmawuli@st.ug.edu.gh>
2025-08-07 23:46:46 +0100

    Application demonstrating OOP concepts such as method overriding and inheritance

---
+++ animal_oop_application/animal_oop_application/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace AnimalSounds
+{
+
+    //Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal sound");
+        }
+    }
+    //Derived class
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark!");
+        }
+    }
+    //Derived class
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //Instantiate objects of the various derived classes
+
+                Animal animal = new Animal();
+                Animal dog = new Dog();
+                Animal cat = new Cat();
+
+                //Calling the MakeSound methods
+                animal.MakeSound();
+                dog.MakeSound();
+                cat.MakeSound();
+
+            }
+
+
+        }
+
+
+    }
+
+
+
+
+
+}
